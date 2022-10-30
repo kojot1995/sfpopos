28c93329f7a7363774d08bc3b56a1ff98015af10
kojot1995 <wojak.karol95@gmail.com>
2022-10-30 12:43:00 +0100

    Add search bar to filter Spaces by title or address

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
-import data from '../../sfpopos-data.json';
+import data from '../../sfpopos-data.js'; 
 
 function POPOSList() {
 
-  const spaces = data.map(({title, address, images}, index) => {
+  const [input, setInput] = useState(''); // Add React state to function component
+  const spaces = data.filter(obj => obj.title.toLowerCase().includes(input.toLowerCase()) || obj.address.toLowerCase().includes(input.toLowerCase())).map((obj) => {
 
     return (
       <POPOSSpace 
-        id={index}
-        key={title}
-        name={title}
-        address={address}
-        image={images[0]}
+        id={obj.id}
+        key={obj.title}
+        name={obj.title}
+        address={obj.address}
+        image={obj.images[0]}
       />
     )
   })
 
     return (
       <div className='POPOSList'>
+        <form>
+          <input 
+            value={input}
+            placeholder='Search for spaces...'
+            onChange={e => { setInput(e.target.value)} }
+          />
+        </form>
         {spaces} 
       </div>
     )
