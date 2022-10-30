983d5af5785d370ac97f4cd4ef1c6b7a024ce28c
kojot1995 <wojak.karol95@gmail.com>
2022-10-30 11:48:08 +0100

    Improve styling

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import data from '../../sfpopos-data.json';
 import { useParams } from 'react-router-dom';
+import './POPOSDetails.css';
 
 function POPOSDetails() {
   const { id } = useParams();
   const { images, title, desc, hours, features, geo } = data[id];
+
   return (
-    <div>
-      <div>
-        <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} />
+    <div className='POPOSDetails'>
+      <div className='POPOSDetails-image'>
+        <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt='San Franciso POPOS' />
       </div>
-      <div>
-        <h1>{title}</h1>
-        <p>{desc}</p>
-        <p>Opening hours: {hours}</p>
-        <p>Features: {features.map(feature => feature + ' ')}</p>
-        <p>Coordinates: {geo.lat} {geo.lon}</p>
+      <div className='POPOSDetails-info'>
+        <h1 className='POPOSDetails-title'>{title}</h1>
+        <p className='POPOSDetails-desc'>{desc}</p>
+        <p className='POPOSDetails-hours'>Opening hours:<br/> {hours}</p>
+        <p className='POPOSDetails-features'>Features: {features.map(feature => feature + ' ')}</p>
+        <p className='POPOSDetails-geo'>Coordinates: {geo.lat} {geo.lon}</p>
       </div>
     </div>
   )
